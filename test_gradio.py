bb13fbdce9ee23140a3850823c7d0c8f9a2516f9
nothingcjh-ship-it <240719908+nothingcjh-ship-it@users.noreply.github.com>
2026-02-05 22:33:07 +0800

    Cleanup: Remove legacy files and update .gitignore for cleaner distribution

--- test_gradio.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-def greet(name):
-    return "Hello " + name + "!"
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-print("Launching Gradio test...")
-demo.launch(server_name="127.0.0.1", server_port=8000)
